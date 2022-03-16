315aae638d6e84399a61a02ac6585a5b2a365b20
ahmyi <cryptoamity@gmail.com>
2022-03-16 22:48:50 +0800

    Username checking not only at group

--- src/middleware/memberMiddleware.js
+++ src/middleware/memberMiddleware.js
@@ -13,27 +13,24 @@ class MemberMiddleware extends Middleware {
     async run(ctx, next) {
         if(ctx.test) return;
       
-        if (!ctx || !ctx.appRequest || !ctx.appRequest.is.group || ctx.appRequest.is.action) {
-            if (next) {
-                return next();
-            }
+        if (!ctx || !ctx.appRequest || ctx.appRequest.is.action) {
+            if (next) next();
             return;
-        }
-        const User = Model.LoadRegistry("User");
-        const userId = ctx.from.id;
-        if (!await User.exists(userId)){
-            
-            if (next) {
-                return next();
-            }
-            return;
-        }
-        const username = await User.getUsernameById(userId);
-        if(username !== ctx.from.username) {
-            await User.updateUsername(userId, ctx.from.username);
         }
         
-        await Model.LoadRegistry("Member").addMember(ctx.chat.id, userId);
+        const User = Model.LoadRegistry("User");
+        const userId = ctx.from.id;
+        if (await User.exists(userId)){
+            const username = await User.getUsernameById(userId);
+            if(username !== ctx.from.username) {
+                await User.updateUsername(userId, ctx.from.username);
+            }
+            if(ctx.appRequest.is.group ) {
+                await Model.LoadRegistry("Member").addMember(ctx.chat.id, userId);
+            }
+        }
+        
+        
         // global.log("info",logSystem,"Chat id %d updated for member id %d" , [ctx.chat.id, ctx.from.id]);
         if (next) {
             return next();
