9cce665b5a0e4edf9b790d3f8d5d159d8c614e11
Azizul Hakimi Mohd Yussuf Izzudin <ahmyi@users.noreply.github.com>
2022-03-29 15:36:16 +0800

    Update user.js
    Repair username changed

--- src/models/queries/redis/user.js
+++ src/models/queries/redis/user.js
@@ -10,26 +10,26 @@ class User  extends Query
 		const ukey = [global.config.coin, 'Users' , id].join(':');
 		
 		if(!~this.fields.indexOf(field)) {
-        	return STATUS.ERROR_MODEL_PROPERTIES_NOT_AVALIABLE;
-        }
+        		return STATUS.ERROR_MODEL_PROPERTIES_NOT_AVALIABLE;
+		}
 
-		const user = await global.redisClient.hset(ukey, field, value);
+			const user = await global.redisClient.hset(ukey, field, value);
 
-        if(!user) {
-        	return STATUS.ERROR_MODEL_UPDATE;
-        }
+		if(!user) {
+			return STATUS.ERROR_MODEL_UPDATE;
+		}
 
-        return STATUS.OK;
+        	return STATUS.OK;
 	}
 
 
 	async findByUsername(username) {
-        const aKey = [global.config.coin, "Alias"].join(':');
-        const user_id = await global.redisClient.hget(aKey, username);
-        if(!user_id) {
-        	return null;
-        }
-        return await this.findById(user_id);
+		const aKey = [global.config.coin, "Alias"].join(':');
+		const user_id = await global.redisClient.hget(aKey, username);
+		if(!user_id) {
+			return null;
+		}
+		return await this.findById(user_id);
 	}
 
 	async getUsernameById(user_id) {
@@ -70,60 +70,61 @@ class User  extends Query
 	}
 
 	async remove(user_id, username) {
-        const uKey = [global.config.coin, "Users" ,user_id].join(':');
-        const aKey = [global.config.coin, "Alias"].join(':');
+		const uKey = [global.config.coin, "Users" ,user_id].join(':');
+		const aKey = [global.config.coin, "Alias"].join(':');
 
-        await global.redisClient.multi()
-        .del(uKey)
-        .hdel(aKey, username)
-        .exec();
+		await global.redisClient.multi()
+		.del(uKey)
+		.hdel(aKey, username)
+		.exec();
 
-        return STATUS.OK;
+		return STATUS.OK;
 	}
 	
 	async updateUsername(user_id, username) {
 		const uKey = [global.config.coin, "Users" ,user_id].join(':');
-        const aKey = [global.config.coin, "Alias"].join(':');
+		const aKey = [global.config.coin, "Alias"].join(':');
 
-        let oldUsername = await global.redisClient.hget(uKey, username);
+		let oldUsername = await global.redisClient.hget(uKey, username);
 
-        return await global.redisClient.hset(uKey, 'username', username)
-        .hdel(aKey, username)
-        .hset(aKey, username, user_id)
-        .exec();
+		return await global.redisClient
+			.hset(uKey, 'username', username)
+			.hdel(aKey, oldUsername)
+			.hset(aKey, username, user_id)
+			.exec();
 	}
 
 
 	async add(user_id, username) {
-        const uKey = [global.config.coin, "Users" ,user_id].join(':');
-        const aKey = [global.config.coin, "Alias"].join(':');
+		const uKey = [global.config.coin, "Users" ,user_id].join(':');
+		const aKey = [global.config.coin, "Alias"].join(':');
 
-        let user = await this.findById(user_id);
-                
-		if(user && parseInt(user.user_id) === user_id) {
-			
-			if(user.wallet) {
-				return  STATUS.ERROR_ACCOUNT_EXISTS;
-	        }
-	        user.status = STATUS.WALLET_REQUIRED;
-			return  user;
-        }
-        const status = STATUS.WALLET_REQUIRED;
-        user = {
-        	user_id,
-        	username,
-        	status
-        }
-        	
-    	let result = await global.redisClient.multi()
-        .hmset(uKey, ["user_id",user_id,
-        	"username",username,
-        	"status",username])
-        .hset(aKey, username, user_id)
-        .exec();
+		let user = await this.findById(user_id);
 
-       
-        return user;
+			if(user && parseInt(user.user_id) === user_id) {
+
+				if(user.wallet) {
+					return  STATUS.ERROR_ACCOUNT_EXISTS;
+			}
+			user.status = STATUS.WALLET_REQUIRED;
+				return  user;
+		}
+		const status = STATUS.WALLET_REQUIRED;
+		user = {
+			user_id,
+			username,
+			status
+		}
+
+		let result = await global.redisClient.multi()
+		.hmset(uKey, ["user_id",user_id,
+			"username",username,
+			"status",username])
+		.hset(aKey, username, user_id)
+		.exec();
+
+
+		return user;
 	}
 }
 
